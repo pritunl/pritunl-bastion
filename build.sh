0b751eb7ad3ad00b6f6ac31f56cde1897ce88cbb
Zachary Huff <zach.huff.386@gmail.com>
2019-10-01 06:50:37 -0400

    Fix build script

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ sudo docker build --rm --no-cache -t pritunl-bastion .
 export date=$(date "+%Y%m%d%H%M%S")
 sudo docker tag pritunl-bastion:latest iad.ocir.io/pritunl8472/pritunl-bastion:"$date"
 sudo docker tag pritunl-bastion:latest iad.ocir.io/pritunl8472/pritunl-bastion:latest
-sudo docker --authfile=/etc/containers/auth.json push iad.ocir.io/pritunl8472/pritunl-bastion:"$date"
-sudo docker --authfile=/etc/containers/auth.json push iad.ocir.io/pritunl8472/pritunl-bastion:latest
+sudo docker push --authfile=/etc/containers/auth.json iad.ocir.io/pritunl8472/pritunl-bastion:"$date"
+sudo docker push --authfile=/etc/containers/auth.json iad.ocir.io/pritunl8472/pritunl-bastion:latest
 echo $date
