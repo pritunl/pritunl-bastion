e56dfe1c4cb6b6e068a2678aedc8e63ed393ee26
Zachary Huff <zach.huff.386@gmail.com>
2019-10-01 06:46:51 -0400

    Update build

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 set -e
 
 sudo docker pull oraclelinux:7
-sudo docker build --rm -t pritunl-bastion .
+sudo docker build --rm --no-cache -t pritunl-bastion .
 
 export date=$(date "+%Y%m%d%H%M%S")
-sudo docker tag pritunl-bastion:latest pritunl/pritunl-bastion:"$date"
-sudo docker tag pritunl-bastion:latest pritunl/pritunl-bastion:latest
-sudo docker push pritunl/pritunl-bastion:"$date"
-sudo docker push pritunl/pritunl-bastion:latest
+sudo docker tag pritunl-bastion:latest iad.ocir.io/pritunl8472/pritunl-bastion:"$date"
+sudo docker tag pritunl-bastion:latest iad.ocir.io/pritunl8472/pritunl-bastion:latest
+sudo docker push iad.ocir.io/pritunl8472/pritunl-bastion:"$date"
+sudo docker push iad.ocir.io/pritunl8472/pritunl-bastion:latest
 echo $date
