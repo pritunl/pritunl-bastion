8e7aacf4672528bc75d90f01eca0f38a2318eef7
Zachary Huff <zach.huff.386@gmail.com>
2019-10-04 17:03:48 -0400

    Add github packages to build

--- build.sh
+++ build.sh
@@ -5,8 +5,15 @@ sudo docker pull oraclelinux:7
 sudo docker build --rm --no-cache -t pritunl-bastion .
 
 export date=$(date "+%Y%m%d%H%M%S")
+
 sudo docker tag pritunl-bastion:latest iad.ocir.io/pritunl8472/pritunl-bastion:"$date"
 sudo docker tag pritunl-bastion:latest iad.ocir.io/pritunl8472/pritunl-bastion:latest
 sudo docker push --authfile=/etc/containers/auth.json iad.ocir.io/pritunl8472/pritunl-bastion:"$date"
 sudo docker push --authfile=/etc/containers/auth.json iad.ocir.io/pritunl8472/pritunl-bastion:latest
+
+sudo docker tag pritunl-bastion:latest docker.pkg.github.com/pritunl/pritunl-zero:"$date"
+sudo docker tag pritunl-bastion:latest docker.pkg.github.com/pritunl/pritunl-zero:latest
+sudo docker push --authfile=/etc/containers/auth.json docker.pkg.github.com/pritunl/pritunl-zero:"$date"
+sudo docker push --authfile=/etc/containers/auth.json docker.pkg.github.com/pritunl/pritunl-zero:latest
+
 echo $date
